ced43ba6199c71fd492445f0e710f30c9f680f15
bar-kakon <barkakon5@gmail.com>
2026-08-29 20:56:28 +0300

    chore(screens): delete the static prototypes their React screens replaced

--- Frontend/screens/validation.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-(function () {
-    const fields = document.querySelectorAll('.form-input, .form-select, .checkbox-input');
-    fields.forEach((field) => {
-        const markTouched = () => field.classList.add('touched');
-        field.addEventListener('blur', markTouched);
-        field.addEventListener('change', markTouched);
-    });
-})();
